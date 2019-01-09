544f63fc24671546eab7e83de80f7167113af69c
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-01-09 08:29:21 +0000

    # TOEI-105 ### Changed - 勘定科目インターフェースを調整

--- src/service/accountTitle.ts
+++ src/service/accountTitle.ts
@@ -8,7 +8,117 @@ import { Service } from '../service';
  */
 export class AccountTitleService extends Service {
     /**
-     * 科目作成
+     * 科目分類作成
+     */
+    public async createAccounTitleCategory(
+        params: factory.accountTitle.IAccountTitle
+    ): Promise<factory.accountTitle.IAccountTitle> {
+        return this.fetch({
+            uri: '/accountTitles/accountTitleCategory',
+            method: 'POST',
+            body: params,
+            expectedStatusCodes: [CREATED]
+        }).then(async (response) => response.json());
+    }
+
+    /**
+     * 科目分類検索
+     */
+    public async searchAccountTitleCategories(params: factory.accountTitle.ISearchConditions): Promise<{
+        totalCount: number;
+        data: factory.accountTitle.IAccountTitle[];
+    }> {
+        return this.fetch({
+            uri: '/accountTitles/accountTitleCategory',
+            method: 'GET',
+            qs: params,
+            expectedStatusCodes: [OK]
+        }).then(async (response) => {
+            return {
+                totalCount: Number(<string>response.headers.get('X-Total-Count')),
+                data: await response.json()
+            };
+        });
+    }
+
+    /**
+     * 科目分類更新
+     */
+    public async updateAccounTitleCategory(params: factory.accountTitle.IAccountTitle): Promise<void> {
+        await this.fetch({
+            uri: `/accountTitles/accountTitleCategory/${params.codeValue}`,
+            method: 'PUT',
+            body: params,
+            expectedStatusCodes: [NO_CONTENT]
+        });
+    }
+
+    /**
+     * 科目分類削除
+     * 不可逆的な物理削除です
+     */
+    public async deleteAccounTitleCategoryByCodeValue(params: {
+        codeValue: string;
+    }): Promise<void> {
+        await this.fetch({
+            uri: `/accountTitles/accountTitleCategory/${params.codeValue}`,
+            method: 'DELETE',
+            expectedStatusCodes: [NO_CONTENT]
+        });
+    }
+
+    /**
+     * 科目追加
+     */
+    public async createAccounTitleSet(
+        params: factory.accountTitle.IAccountTitle
+    ): Promise<factory.accountTitle.IAccountTitle> {
+        if (params.inCodeSet === undefined) {
+            throw new factory.errors.ArgumentNull('inCodeSet');
+        }
+
+        return this.fetch({
+            uri: '/accountTitles/accountTitleSet',
+            method: 'POST',
+            body: params,
+            expectedStatusCodes: [CREATED]
+        }).then(async (response) => response.json());
+    }
+
+    /**
+     * 科目検索
+     */
+    public async searchAccountTitleSets(params: factory.accountTitle.ISearchConditions): Promise<{
+        totalCount: number;
+        data: factory.accountTitle.IAccountTitle[];
+    }> {
+        return this.fetch({
+            uri: '/accountTitles/accountTitleSet',
+            method: 'GET',
+            qs: params,
+            expectedStatusCodes: [OK]
+        }).then(async (response) => {
+            return {
+                totalCount: Number(<string>response.headers.get('X-Total-Count')),
+                data: await response.json()
+            };
+        });
+    }
+
+    /**
+     * 科目更新
+     */
+    public async updateAccounTitleSet(params: factory.accountTitle.IAccountTitle): Promise<void> {
+        await this.fetch({
+            uri: `/accountTitles/accountTitleSet/${params.codeValue}`,
+            method: 'PUT',
+            body: params,
+            expectedStatusCodes: [NO_CONTENT]
+        });
+    }
+
+    /**
+     * 細目追加
      */
     public async create(
         params: factory.accountTitle.IAccountTitle
@@ -22,7 +132,7 @@ export class AccountTitleService extends Service {
     }
 
     /**
-     * 科目検索
+     * 細目検索
      */
     public async search(params: factory.accountTitle.ISearchConditions): Promise<{
         totalCount: number;
@@ -38,45 +148,6 @@ export class AccountTitleService extends Service {
                 totalCount: Number(<string>response.headers.get('X-Total-Count')),
                 data: await response.json()
             };
-        });
-    }
-
-    /**
-     * コードで科目検索
-     */
-    public async findByIdentifier(params: {
-        identifier: string;
-    }): Promise<factory.accountTitle.IAccountTitle> {
-        return this.fetch({
-            uri: `/accountTitles/${params.identifier}`,
-            method: 'GET',
-            expectedStatusCodes: [OK]
-        }).then(async (response) => response.json());
-    }
-
-    /**
-     * 科目更新
-     */
-    public async update(params: factory.accountTitle.IAccountTitle): Promise<void> {
-        await this.fetch({
-            uri: `/accountTitles/${params.identifier}`,
-            method: 'PUT',
-            body: params,
-            expectedStatusCodes: [NO_CONTENT]
-        });
-    }
-
-    /**
-     * 科目削除
-     * 不可逆的な物理削除です
-     */
-    public async deleteByIdentifier(params: {
-        identifier: string;
-    }): Promise<void> {
-        await this.fetch({
-            uri: `/accountTitles/${params.identifier}`,
-            method: 'DELETE',
-            expectedStatusCodes: [NO_CONTENT]
         });
     }
 }
