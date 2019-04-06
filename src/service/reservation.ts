7d064f4ce968555febea3e9624e7624e4d1d4a36
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-04-06 23:22:08 +0000

    ### Changed - 予約検索条件拡張(チケット、チケットホルダー)

--- src/service/reservation.ts
+++ src/service/reservation.ts
@@ -32,29 +32,6 @@ export class ReservationService extends Service {
     }
 
     /**
-     * 上映イベント予約検索
-     * @deprecated Use search()
-     */
-    public async searchScreeningEventReservations(
-        params: factory.reservation.ISearchConditions<factory.reservationType.EventReservation>
-    ): Promise<{
-        totalCount: number;
-        data: IEventReservation[];
-    }> {
-        return this.fetch({
-            uri: '/reservations/eventReservation/screeningEvent',
-            method: 'GET',
-            qs: params,
-            expectedStatusCodes: [OK]
-        }).then(async (response) => {
-            return {
-                totalCount: Number(<string>response.headers.get('X-Total-Count')),
-                data: await response.json()
-            };
-        });
-    }
-
-    /**
      * IDで予約検索
      */
     public async findById<T extends factory.reservationType>(params: {
@@ -62,21 +39,6 @@ export class ReservationService extends Service {
     }): Promise<factory.reservation.IReservation<T>> {
         return this.fetch({
             uri: `/reservations/${params.id}`,
-            method: 'GET',
-            qs: params,
-            expectedStatusCodes: [OK]
-        }).then(async (response) => response.json());
-    }
-
-    /**
-     * IDで上映イベント予約検索
-     * @deprecated Use findById()
-     */
-    public async findScreeningEventReservationById(params: {
-        id: string;
-    }): Promise<IEventReservation> {
-        return this.fetch({
-            uri: `/reservations/eventReservation/screeningEvent/${params.id}`,
             method: 'GET',
             qs: params,
             expectedStatusCodes: [OK]
