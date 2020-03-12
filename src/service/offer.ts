a320c762c8ff5a2f34b841eb235563745d93a02d
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-03-12 13:53:30 +0000

    ### Changed - ticketTypesコレクションをoffersコレクションへ移行

--- src/service/offer.ts
+++ src/service/offer.ts
@@ -8,76 +8,6 @@ import { ISearchResult, Service } from '../service';
  */
 export class OfferService extends Service {
     /**
-     * 券種作成
-     */
-    public async createTicketType(
-        params: factory.offer.IUnitPriceOffer
-    ): Promise<factory.offer.IUnitPriceOffer> {
-        return this.fetch({
-            uri: '/ticketTypes',
-            method: 'POST',
-            body: params,
-            expectedStatusCodes: [CREATED]
-        }).then(async (response) => response.json());
-    }
-
-    /**
-     * 券種検索
-     */
-    public async searchTicketTypes(
-        params: factory.offer.ISearchConditions
-    ): Promise<ISearchResult<factory.offer.IUnitPriceOffer[]>> {
-        return this.fetch({
-            uri: '/ticketTypes',
-            method: 'GET',
-            qs: params,
-            expectedStatusCodes: [OK]
-        }).then(async (response) => {
-            return {
-                data: await response.json()
-            };
-        });
-    }
-
-    /**
-     * IDで券種検索
-     */
-    public async findTicketTypeById(params: {
-        id: string;
-    }): Promise<factory.offer.IUnitPriceOffer> {
-        return this.fetch({
-            uri: `/ticketTypes/${encodeURIComponent(String(params.id))}`,
-            method: 'GET',
-            expectedStatusCodes: [OK]
-        }).then(async (response) => response.json());
-    }
-
-    /**
-     * 券種更新
-     */
-    public async updateTicketType(params: factory.offer.IUnitPriceOffer): Promise<void> {
-        await this.fetch({
-            uri: `/ticketTypes/${encodeURIComponent(String(params.id))}`,
-            method: 'PUT',
-            body: params,
-            expectedStatusCodes: [NO_CONTENT]
-        });
-    }
-
-    /**
-     * 券種削除
-     */
-    public async deleteTicketType(params: {
-        id: string;
-    }): Promise<void> {
-        await this.fetch({
-            uri: `/ticketTypes/${encodeURIComponent(String(params.id))}`,
-            method: 'DELETE',
-            expectedStatusCodes: [NO_CONTENT]
-        });
-    }
-
-    /**
      * オファー作成
      */
     public async create(
@@ -120,7 +50,7 @@ export class OfferService extends Service {
     /**
      * オファー更新
      */
-    public async updateOffer(params: factory.offer.IUnitPriceOffer): Promise<void> {
+    public async update(params: factory.offer.IUnitPriceOffer): Promise<void> {
         await this.fetch({
             uri: `/offers/${encodeURIComponent(String(params.id))}`,
             method: 'PUT',
@@ -132,9 +62,7 @@ export class OfferService extends Service {
     /**
      * オファー削除
      */
-    public async deleteOffer(params: {
-        id: string;
-    }): Promise<void> {
+    public async deleteById(params: { id: string }): Promise<void> {
         await this.fetch({
             uri: `/offers/${encodeURIComponent(String(params.id))}`,
             method: 'DELETE',
