fcaf97b338ec86b60bce6b807be6af8797c98cdd
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-03-31 06:09:40 +0000

    ### Added - 座席作成を追加 - 座席削除を追加 - スクリーン作成を追加 - スクリーン削除を追加 - スクリーンセクション作成を追加 - スクリーンセクション検索を追加 - スクリーンセクション更新を追加 - スクリーンセクション削除を追加

--- src/service/place.ts
+++ src/service/place.ts
@@ -29,7 +29,6 @@ export class PlaceService extends Service {
     public async searchMovieTheaters(
         params: factory.place.movieTheater.ISearchConditions
     ): Promise<{
-        totalCount: number;
         data: factory.place.movieTheater.IPlaceWithoutScreeningRoom[];
     }> {
         return this.fetch({
@@ -39,7 +38,6 @@ export class PlaceService extends Service {
             expectedStatusCodes: [OK]
         }).then(async (response) => {
             return {
-                totalCount: Number(<string>response.headers.get('X-Total-Count')),
                 data: await response.json()
             };
         });
@@ -84,12 +82,23 @@ export class PlaceService extends Service {
     }
 
     /**
+     * スクリーン作成
+     */
+    public async createScreeningRoom(params: factory.place.screeningRoom.IPlace): Promise<factory.place.screeningRoom.IPlace> {
+        return this.fetch({
+            uri: `/places/${factory.placeType.ScreeningRoom}`,
+            method: 'POST',
+            body: params,
+            expectedStatusCodes: [CREATED]
+        }).then(async (response) => response.json());
+    }
+
+    /**
      * スクリーン検索
      */
     public async searchScreeningRooms(
         params: any
     ): Promise<{
-        totalCount?: number;
         data: factory.place.screeningRoom.IPlace[];
     }> {
         return this.fetch({
@@ -117,12 +126,106 @@ export class PlaceService extends Service {
     }
 
     /**
+     * スクリーン削除
+     */
+    public async deleteScreeningRoom(params: {
+        project: { id: string };
+        branchCode: string;
+        containedInPlace: { branchCode: string };
+    }): Promise<void> {
+        await this.fetch({
+            uri: `/places/${factory.placeType.ScreeningRoom}/${encodeURIComponent(String(params.branchCode))}`,
+            method: 'DELETE',
+            body: params,
+            expectedStatusCodes: [NO_CONTENT]
+        });
+    }
+
+    /**
+     * スクリーンセクション作成
+     */
+    public async createScreeningRoomSection(
+        params: factory.place.screeningRoomSection.IPlace
+    ): Promise<factory.place.screeningRoomSection.IPlace> {
+        return this.fetch({
+            uri: `/places/${factory.placeType.ScreeningRoomSection}`,
+            method: 'POST',
+            body: params,
+            expectedStatusCodes: [CREATED]
+        }).then(async (response) => response.json());
+    }
+
+    /**
+     * スクリーンセクション検索
+     */
+    public async searchScreeningRoomSections(
+        params: any
+    ): Promise<{
+        data: factory.place.screeningRoomSection.IPlace[];
+    }> {
+        return this.fetch({
+            uri: `/places/${factory.placeType.ScreeningRoomSection}`,
+            method: 'GET',
+            qs: params,
+            expectedStatusCodes: [OK]
+        }).then(async (response) => {
+            return {
+                data: await response.json()
+            };
+        });
+    }
+
+    /**
+     * スクリーンセクション更新
+     */
+    public async updateScreeningRoomSection(params: factory.place.screeningRoomSection.IPlace): Promise<void> {
+        await this.fetch({
+            uri: `/places/${factory.placeType.ScreeningRoomSection}/${encodeURIComponent(String(params.branchCode))}`,
+            method: 'PUT',
+            body: params,
+            expectedStatusCodes: [NO_CONTENT]
+        });
+    }
+
+    /**
+     * スクリーンセクション削除
+     */
+    public async deleteScreeningRoomSection(params: {
+        project: { id: string };
+        branchCode: string;
+        containedInPlace: {
+            branchCode: string;
+            containedInPlace: {
+                branchCode: string;
+            };
+        };
+    }): Promise<void> {
+        await this.fetch({
+            uri: `/places/${factory.placeType.ScreeningRoomSection}/${encodeURIComponent(String(params.branchCode))}`,
+            method: 'DELETE',
+            body: params,
+            expectedStatusCodes: [NO_CONTENT]
+        });
+    }
+
+    /**
+     * スクリーン作成
+     */
+    public async createSeat(params: factory.place.seat.IPlace): Promise<factory.place.seat.IPlace> {
+        return this.fetch({
+            uri: `/places/${factory.placeType.Seat}`,
+            method: 'POST',
+            body: params,
+            expectedStatusCodes: [CREATED]
+        }).then(async (response) => response.json());
+    }
+
+    /**
      * 座席検索
      */
     public async searchSeats(
         params: any
     ): Promise<{
-        totalCount?: number;
         data: factory.place.seat.IPlace[];
     }> {
         return this.fetch({
@@ -144,6 +247,30 @@ export class PlaceService extends Service {
         await this.fetch({
             uri: `/places/${factory.placeType.Seat}/${encodeURIComponent(String(params.branchCode))}`,
             method: 'PUT',
+            body: params,
+            expectedStatusCodes: [NO_CONTENT]
+        });
+    }
+
+    /**
+     * 座席削除
+     */
+    public async deleteSeat(params: {
+        project: { id: string };
+        branchCode: string;
+        containedInPlace: {
+            branchCode: string;
+            containedInPlace: {
+                branchCode: string;
+                containedInPlace: {
+                    branchCode: string;
+                };
+            };
+        };
+    }): Promise<void> {
+        await this.fetch({
+            uri: `/places/${factory.placeType.Seat}/${encodeURIComponent(String(params.branchCode))}`,
+            method: 'DELETE',
             body: params,
             expectedStatusCodes: [NO_CONTENT]
         });
