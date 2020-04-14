4e3c491813667a5a1a533d4bd2e815b6d8f4b502
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-04-14 00:16:25 +0000

    ### Added - プロジェクトサービスを追加

---
+++ src/service/project.ts
@@ -0,0 +1,61 @@
+import { OK } from 'http-status';
+
+import * as factory from '../factory';
+import { Service } from '../service';
+
+export interface IGetHealthResult {
+    version?: string;
+    status?: number;
+    message?: string;
+}
+
+/**
+ * プロジェクトサービス
+ */
+export class ProjectService extends Service {
+    /**
+     * プロジェクト取得
+     */
+    public async findById(params: {
+        id: string;
+    }): Promise<factory.project.IProject> {
+        return this.fetch({
+            uri: `/projects/${params.id}`,
+            method: 'GET',
+            expectedStatusCodes: [OK]
+        })
+            .then(async (response) => response.json());
+    }
+
+    /**
+     * ヘルスチェック
+     */
+    public async getHealth(_: any): Promise<IGetHealthResult> {
+        return this.fetch({
+            uri: '/health',
+            method: 'GET',
+            expectedStatusCodes: [OK]
+        })
+            .then(async (response) => {
+                const version = response.headers.get('X-API-Version');
+
+                return {
+                    version: (typeof version === 'string') ? version : undefined,
+                    status: response.status,
+                    message: await response.text()
+                };
+            });
+    }
+
+    /**
+     * DB統計取得
+     */
+    public async getDBStats(_: any): Promise<any> {
+        return this.fetch({
+            uri: '/stats/dbStats',
+            method: 'GET',
+            expectedStatusCodes: [OK]
+        })
+            .then(async (response) => response.json());
+    }
+}
