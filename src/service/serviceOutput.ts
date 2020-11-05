a1a205a4116e25c154d050dcc2f581a1da87227b
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-11-05 02:37:37 +0000

    - 複数対応のサービスアウトプット識別子発行を追加

--- src/service/serviceOutput.ts
+++ src/service/serviceOutput.ts
@@ -1,13 +1,27 @@
-import { OK } from 'http-status';
-
-// import * as factory from '../factory';
+import { CREATED, OK } from 'http-status';
 
 import { Service } from '../service';
+
+export interface IPublishIdentifierResult {
+    identifier: string;
+}
 
 /**
  * サービスアウトプットサービス
  */
 export class ServiceOutputService extends Service {
+    /**
+     * 識別子発行
+     */
+    public async publishIdentifier(params: [{ project: { id: string } }]): Promise<IPublishIdentifierResult[]> {
+        return this.fetch({
+            uri: '/serviceOutputs/identifier',
+            method: 'POST',
+            body: params,
+            expectedStatusCodes: [CREATED]
+        }).then(async (response) => response.json());
+    }
+
     /**
      * 検索
      */
